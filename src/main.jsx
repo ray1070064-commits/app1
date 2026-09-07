72e9f63462c90c1d1be074f95c1b714fa553a0d5
ray1070064-commits <ray1070064@gmail.com>
2026-09-07 19:31:57 +0800

    style: load life management styles v182

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './styles.css'
 import './market.css'
 import './info.css'
+import './life.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
