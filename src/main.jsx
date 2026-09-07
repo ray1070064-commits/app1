461d43a793518ade615808afa880af8ddb578bfa
ray1070064-commits <ray1070064@gmail.com>
2026-09-08 02:26:29 +0800

    Load P0 gameplay styles

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import './feature.css'
 import './tutorial.css'
 import './settlement.css'
 import './parity.css'
+import './p0.css'
 import './ui-system.css'
 
 createRoot(document.getElementById('root')).render(
